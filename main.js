0bf6abd1ccbe77612f65afa956011e04ffd427ab
Bram van Osta <bramvanosta@gmail.com>
2021-03-08 11:54:28 +0100

    :sparkles: Add JS todo creation

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+function addTodoElement(title) {
+  // Nous récupérons l'élément qui liste toutes les tâches
+  const todoList = document.getElementById("todoList");
+
+  // Nous créons un nouvel élément qui va représenter notre tâche
+  const element = document.createElement("div");
+  element.classList.add("Todo__item");
+
+  // Nous ajoutons un titre à notre tâche
+  const titleElement = document.createElement("div");
+  titleElement.classList.add("Todo__item-label");
+  titleElement.innerHTML = title;
+
+  // Nous ajoutons le titre à notre tâche
+  element.append(titleElement);
+
+  // Nous ajoutons la nouvelle tâche dans la liste des tâches
+  todoList.append(element);
+}
+
+document.getElementById("todoAdd").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // Nous récupérons la valeur que nous avons saisi
+  const title = document.getElementById("todoTitle").value;
+
+  // Nous appelons la fonction qui ajoute une nouvelle tâche à la liste
+  addTodoElement(title);
+
+  // Nous réinitialisons le titre
+  document.getElementById("todoTitle").value = "";
+});
